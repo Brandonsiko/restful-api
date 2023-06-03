5c06cdc71d304983be62925e14c281ad539e2337
Jayson <brabdonsiko3@gmail.com>
2023-06-04 00:34:14 +0200

    added documentation with regards to what is needed

--- Rest-Api.py
+++ Rest-Api.py
@@ -1,1 +1,10 @@
 import requests
+import json
+
+response=requests.get('http://wethinkcode.co.za')
+print(response)
+
+#remember we are using flask
+#we are also using Rest api 
+
+"""Make a api that gets all university names and all the courses they offer and use this api in live demo session of the actual app that you are building"""
